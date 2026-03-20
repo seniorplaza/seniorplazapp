9f93a30b2ba756837a1ac751dcbcafaaf24684a1
seniorplaza <seniorplazaml@gmail.com>
2026-03-20 15:49:16 +0100

    RESTAURACIÓN APP/APK

--- android/app/src/main/java/com/seniorplaza/app/MainActivity.java
+++ android/app/src/main/java/com/seniorplaza/app/MainActivity.java
@@ -7,6 +7,7 @@ import com.capacitorjs.plugins.statusbar.StatusBarPlugin;
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_NoActionBar);
         registerPlugin(StatusBarPlugin.class);
         super.onCreate(savedInstanceState);
     }
